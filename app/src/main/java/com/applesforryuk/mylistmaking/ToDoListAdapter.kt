1b7b23a652f99ccb4812d4534b3e0d9f5b30b4c9
ApplesForRyuk <murtov21@gmail.com>
2022-11-25 20:17:31 +0200

    Finishing the ViewHolder

--- app/src/main/java/com/applesforryuk/mylistmaking/ToDoListAdapter.kt
+++ app/src/main/java/com/applesforryuk/mylistmaking/ToDoListAdapter.kt
@@ -1,13 +1,17 @@
 package com.applesforryuk.mylistmaking
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class ToDoListAdapter : RecyclerView.Adapter<ToDoListViewHolder>() {
 
     private val  toDoLists = arrayOf("Android Development", "House work", "Errands")
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoListViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.todo_list_view_holder, parent, false )
+        return ToDoListViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ToDoListViewHolder, position: Int) {
