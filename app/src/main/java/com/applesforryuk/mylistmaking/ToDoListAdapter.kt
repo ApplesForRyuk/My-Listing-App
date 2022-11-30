7561045df98822cf30d83c546eb02a683887ab5b
ApplesForRyuk <murtov21@gmail.com>
2022-11-30 22:52:58 +0200

    read/save task lists

--- app/src/main/java/com/applesforryuk/mylistmaking/ToDoListAdapter.kt
+++ app/src/main/java/com/applesforryuk/mylistmaking/ToDoListAdapter.kt
@@ -7,17 +7,6 @@ import androidx.recyclerview.widget.RecyclerView
 
 class ToDoListAdapter(val lists: ArrayList<TaskList>) : RecyclerView.Adapter<ToDoListViewHolder>() {
 
-    //private var  toDoLists = mutableListOf("Android Development", "House work", "Errands")
-
-
-   /* fun addNewItem(listName: String = "") {
-        if (listName.isEmpty()) {
-            toDoLists.add("Todo List" + (toDoLists.size +1))
-        } else {
-            toDoLists.add(listName)
-        }
-        notifyDataSetChanged()
-    } */
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoListViewHolder {
         val view = LayoutInflater.from(parent.context)
