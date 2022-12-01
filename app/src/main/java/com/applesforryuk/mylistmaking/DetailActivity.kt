a4aa7dcd4b795ba4a662c79da7d2145af3dfcc1d
ApplesForRyuk <murtov21@gmail.com>
2022-12-01 23:23:09 +0200

    feat: recycler view for DetailActivity

--- app/src/main/java/com/applesforryuk/mylistmaking/DetailActivity.kt
+++ app/src/main/java/com/applesforryuk/mylistmaking/DetailActivity.kt
@@ -2,17 +2,25 @@ package com.applesforryuk.mylistmaking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.applesforryuk.mylistmaking.databinding.ActivityDetailBinding
 
 
 class DetailActivity : AppCompatActivity() {
 
     private var list: TaskList? = null
 
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         list = intent.getParcelableExtra(MainActivity.INTENT_LIST_KEY) as TaskList?
         title = list?.name
+
+        binding.taskListRecyclerview.layoutManager = LinearLayoutManager(this)
+
+
 
     }
 }
