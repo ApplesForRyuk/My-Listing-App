dee67717a336bbb794e919ec2bb356f7b3bfe0d7
ApplesForRyuk <murtov21@gmail.com>
2022-12-21 21:12:57 +0200

    feat: splash screen and minor changes in manifest

--- app/src/main/java/com/applesforryuk/mylistmaking/MainActivity.kt
+++ app/src/main/java/com/applesforryuk/mylistmaking/MainActivity.kt
@@ -2,6 +2,7 @@ package com.applesforryuk.mylistmaking
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
 import com.applesforryuk.mylistmaking.databinding.ActivityMainBinding
 
@@ -11,6 +12,7 @@ class MainActivity :
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
